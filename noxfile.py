f80f859f24ed691280a3007905589ace386f5bf1
epenet <6771947+epenet@users.noreply.github.com>
2026-01-08 13:13:13 +0100

    Drop xdoctest

--- noxfile.py
+++ noxfile.py
@@ -31,7 +31,6 @@ nox.options.sessions = (
     "mypy",
     "tests",
     "typeguard",
-    "xdoctest",
     "docs-build",
 )
 
@@ -192,21 +191,6 @@ def typeguard(session: Session) -> None:
     session.install(".[cli]")
     session.install("pytest", "typeguard", "pygments", "pytest-asyncio")
     session.run("pytest", f"--typeguard-packages={package}", *session.posargs)
-
-
-@session(python=python_versions)
-def xdoctest(session: Session) -> None:
-    """Run examples with xdoctest."""
-    if session.posargs:
-        args = [package, *session.posargs]
-    else:
-        args = [f"--modname={package}", "--command=all"]
-        if "FORCE_COLOR" in os.environ:
-            args.append("--colored=1")
-
-    session.install(".")
-    session.install("xdoctest[colors]")
-    session.run("python", "-m", "xdoctest", *args)
 
 
 @session(name="docs-build", python=python_versions[0])
